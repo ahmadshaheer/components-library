518ca9a9986256bc3bd4145a8d3c9ff2dc4b29d1
ahmadshaheer <ashaheerki@gmail.com>
2024-10-14 20:17:57 +0430

    Updates

---
+++ apps/docs/.storybook/main.js
@@ -0,0 +1,38 @@
+import { dirname, join } from "path";
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+
+const config = {
+  stories: ["../stories/*.stories.tsx", "../stories/**/*.stories.tsx"],
+  addons: [
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-designs"),
+  ],
+  framework: {
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {},
+  },
+
+  core: {},
+
+  async viteFinal(config) {
+    // customize the Vite config here
+    return {
+      ...config,
+      define: { "process.env": {} },
+      resolve: {
+        alias: [],
+      },
+    };
+  },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+  },
+};
+
+export default config;
---
+++ assets/preview-BSWWhxLl.js
@@ -0,0 +1,1 @@
+const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}},t=["autodocs"];export{o as parameters,t as tags};
